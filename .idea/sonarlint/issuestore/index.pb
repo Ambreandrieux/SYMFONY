f747f9f6fd3ce95b2d6cde4731e2b204b0551bb6
Julien PRIGENT <julien.prigent@fnacdarty.com>
2024-01-31 15:58:43 +0100

    Add source

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+D
+config/services.yaml,8\7\87042d1f46b57381244d70e7a42feeb7710727dc
+F
+src/Entity/Product.php,4\3\43672f36815a1d5d48e2d6884176d0a297fbd86d
+4
+.env,3\c\3c84dcdc6bbe3d7817c49dcdc327b926fea1808a
+<
+.php-version,2\b\2bb785ffacc85f8e03db7c9817b453e4494ad2e8
+D
+src/Entity/Brand.php,4\e\4eb56e9b0f554e5d19ea925badcf67e108c124e9
+G
+src/Entity/Category.php,c\8\c85245800948ffb0569f65af912d0e2d467f18f3
---
+++ src/Entity/Category.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+class Category
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    #[ORM\ManyToOne(targetEntity: self::class, inversedBy: 'parentCategory')]
+    private ?self $category = null;
+
+    #[ORM\OneToMany(mappedBy: 'category', targetEntity: self::class)]
+    private Collection $parentCategory;
+
+    #[ORM\OneToMany(mappedBy: 'category', targetEntity: Product::class, orphanRemoval: true)]
+    private Collection $products;
+
+    public function __construct()
+    {
+        $this->parentCategory = new ArrayCollection();
+        $this->products = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getCategory(): ?self
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?self $category): static
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, self>
+     */
+    public function getParentCategory(): Collection
+    {
+        return $this->parentCategory;
+    }
+
+    public function addParentCategory(self $parentCategory): static
+    {
+        if (!$this->parentCategory->contains($parentCategory)) {
+            $this->parentCategory->add($parentCategory);
+            $parentCategory->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeParentCategory(self $parentCategory): static
+    {
+        if ($this->parentCategory->removeElement($parentCategory)) {
+            // set the owning side to null (unless already changed)
+            if ($parentCategory->getCategory() === $this) {
+                $parentCategory->setCategory(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Product>
+     */
+    public function getProducts(): Collection
+    {
+        return $this->products;
+    }
+
+    public function addProduct(Product $product): static
+    {
+        if (!$this->products->contains($product)) {
+            $this->products->add($product);
+            $product->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProduct(Product $product): static
+    {
+        if ($this->products->removeElement($product)) {
+            // set the owning side to null (unless already changed)
+            if ($product->getCategory() === $this) {
+                $product->setCategory(null);
+            }
+        }
+
+        return $this;
+    }
+}
